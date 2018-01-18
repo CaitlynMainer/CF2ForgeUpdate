ec1a82eded7792ae3ea4be47f60b51e15b013140
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2018-01-18 14:09:11 -0600

    .

--- cflatest_conf.dist.php
+++ cflatest_conf.dist.php
@@ -7,6 +7,7 @@ $projects = array(
         "url"=>"https://minecraft.curseforge.com/projects/ModName",
         //Regex to get the version from the jar on curseforge
         //Make sure that the version is in the first capturegroup
+        //This Would match "ModName-MC1.10.2-1.0.1.jar" and extract "1.0.1"
         "regex"=>"ModName-.+-(.+)\.jar",
         //no .json here!
         "cachefile"=>"modname"
